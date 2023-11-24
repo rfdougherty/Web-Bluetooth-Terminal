644f40a441aa3a7f96ea74a5e85da85aad8fd9ff
Bob <robert.f.dougherty@gmail.com>
2023-11-23 19:12:34 -0800

    fix uuid

--- js/main.js
+++ js/main.js
@@ -29,8 +29,8 @@ const logToTerminal = (message, type = '') => {
 };
 
 // Obtain configured instance.
-const terminal = new BluetoothTerminal(serviceUuid="6E400001-B5A3-F393-E0A9-E50E24DCCA9E", //0xFFE0, 
-	                               characteristicUuid="6E400003-B5A3-F393-E0A9-E50E24DCCA9E", // 0xFFE1,
+const terminal = new BluetoothTerminal(serviceUuid="6e400001-b5a3-f393-e0a9-e50e24dcca9e", //0xFFE0, 
+	                               characteristicUuid="6e400003-b5a3-f393-e0a9-e50e24dcca9e", // 0xFFE1,
                                        receiveSeparator='\n', 
 	                               sendSeparator='\n');
 
