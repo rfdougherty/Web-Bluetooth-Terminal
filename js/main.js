d968c2b7a1923612fd9710b5e4e8f89b450450f2
Bob <robert.f.dougherty@gmail.com>
2023-11-23 19:09:35 -0800

    Add service uuid in main`

--- js/main.js
+++ js/main.js
@@ -29,7 +29,10 @@ const logToTerminal = (message, type = '') => {
 };
 
 // Obtain configured instance.
-const terminal = new BluetoothTerminal();
+const terminal = new BluetoothTerminal(serviceUuid="6E400001-B5A3-F393-E0A9-E50E24DCCA9E", //0xFFE0, 
+	                               characteristicUuid="6E400003-B5A3-F393-E0A9-E50E24DCCA9E", // 0xFFE1,
+                                       receiveSeparator='\n', 
+	                               sendSeparator='\n');
 
 // Override `receive` method to log incoming data to the terminal.
 terminal.receive = function(data) {
